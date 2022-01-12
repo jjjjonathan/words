8b4d1dca0efd1eadcd38451f6ecd4a895b563bd9
jjjjonathan <hornathanjon@gmail.com>
2022-01-12 01:29:48 -0800

    Add MUI and bg color to theme

---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#faf5ed',
+    },
+  },
+});
+
+export default theme;
