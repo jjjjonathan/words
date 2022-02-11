377c1e247399e2855a0feb9975c401c7f3d44626
jjjjonathan <hornathanjon@gmail.com>
2022-02-10 20:35:02 -0800

    Revert "Add MUI and bg color to theme"
    This reverts commit 8b4d1dca0efd1eadcd38451f6ecd4a895b563bd9.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../styles/theme';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
