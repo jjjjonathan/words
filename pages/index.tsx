4ff6d6ccd797ffab0da211d64922fc3f96a613d8
jjjjonathan <hornathanjon@gmail.com>
2022-02-10 21:45:19 -0800

    Config ESLint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>words</title>
-        <meta name="description" content="minimalist blog app" />
-      </Head>
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>words</title>
+      <meta name="description" content="minimalist blog app" />
+    </Head>
+  </div>
+);
 
 export default Home;
