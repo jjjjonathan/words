aeb15e8296f8afbf01dfe66d667fdc21d46855a1
jjjjonathan <hornathanjon@gmail.com>
2022-02-11 22:23:11 -0800

    Get serverless up and running

---
+++ server/graphql.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// 'Imported' in serverless.yaml
+exports.graphqlHandler = server.createHandler();
