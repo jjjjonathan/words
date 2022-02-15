f0264371a191c11257bc9a6d118a04346191cf34
jjjjonathan <hornathanjon@gmail.com>
2022-02-15 02:12:38 -0800

    Add config

---
+++ server/config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
