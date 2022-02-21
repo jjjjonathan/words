2d7e68c18acc49636870a563da2927614e2d9caa
jjjjonathan <hornathanjon@gmail.com>
2022-02-20 19:45:29 -0600

    Add Layout component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import Layout from '../components/layout';
 
 const Home: NextPage = () => (
-  <div>
-    <Head>
-      <title>words</title>
-      <meta name="description" content="minimalist blog app" />
-    </Head>
-    <h1>words</h1>
-    <h2>a minimalist blog platform</h2>
-  </div>
+  <Layout title="words">
+    <div>
+      <h1>words</h1>
+      <h2>a minimalist blog platform</h2>
+    </div>
+  </Layout>
 );
 
 export default Home;
