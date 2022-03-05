069c16d3ee5e2127feecc040b586efebcc1c7f98
jjjjonathan <hornathanjon@gmail.com>
2022-03-05 01:49:16 -0600

    Add db to gql context

---
+++ server/context.ts
@@ -0,0 +1,7 @@
+import db from './db';
+
+const context = { db };
+
+export type MyContext = typeof context;
+
+export default context;
--- server/graphql.ts
+++ server/graphql.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from 'apollo-server-lambda';
 import typeDefs from './generated/all-type-defs';
-import resolvers from './resolvers/user';
+import resolvers from './resolvers';
+import context from './context';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
 
 export const graphqlHandler = server.createHandler();
