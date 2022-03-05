bcb5d29cc466fa23d224234a15227ac18d3ca46a
jjjjonathan <hornathanjon@gmail.com>
2022-03-05 00:48:11 -0600

    Write working user query resolver

--- server/graphql.ts
+++ server/graphql.ts
@@ -2,6 +2,9 @@ import { ApolloServer } from 'apollo-server-lambda';
 import typeDefs from './generated/all-type-defs';
 import resolvers from './resolvers/user';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 export const graphqlHandler = server.createHandler();
--- server/resolvers/user.ts
+++ server/resolvers/user.ts
@@ -1,19 +1,42 @@
 import { Resolvers } from '../generated/resolver-types';
+import db from '../db';
+
+type UserQueryResult = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  password_hash: string;
+  email: string;
+  location?: string;
+  bio?: string;
+  created_at: Date;
+  updated_at: Date;
+};
 
 const resolvers: Resolvers = {
   Query: {
-    user: () => ({
-      id: 1,
-      firstName: 'Jonny',
-      lastName: 'Dog',
-      username: 'jonnythedog',
-      email: 'example@example.com',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      location: 'Here',
-      blogs: [],
-      posts: [],
-    }),
+    user: async (parent, { username }) => {
+      const { rows } = await db.query<UserQueryResult>(
+        'SELECT * FROM users WHERE username=$1',
+        [username],
+      );
+      const user = rows[0];
+
+      return {
+        id: user.id,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        username: user.username,
+        email: user.email,
+        location: user.location || undefined,
+        bio: user.bio || undefined,
+        createdAt: user.created_at,
+        updatedAt: user.updated_at,
+        blogs: [],
+        posts: [],
+      };
+    },
   },
 };
 
