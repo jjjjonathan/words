34f5700f79f9ad0028f864ec80f78e24d3b89fbf
jjjjonathan <hornathanjon@gmail.com>
2022-03-06 23:08:19 -0600

    Set heading font weight to inherit

--- client/styles/global.ts
+++ client/styles/global.ts
@@ -32,6 +32,10 @@ const GlobalStyle = createGlobalStyle`
     font-family: var(--font-primary);
     font-weight: var(--font-weight-default);
   }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: inherit;
+  }
 `;
 
 export default GlobalStyle;
