ed7ff0944af7ec2464109e0bc4bfbb57f6e56cf2
jjjjonathan <hornathanjon@gmail.com>
2022-03-12 17:29:05 -0600

    Add header to index

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
 import LoginButton from '../components/LoginButton';
 import Logo from '../components/Logo';
 
+const Header = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
 const Home: NextPage = () => (
   <Layout title="words">
-    <LoginButton />
+    <Header>
+      <LoginButton />
+    </Header>
     <div>
       <Logo />
       <h2>a minimalist blog platform</h2>
