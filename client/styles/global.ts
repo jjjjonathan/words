574aed0f27aacd55eb73b7185bce7c3c8bd3a669
jjjjonathan <hornathanjon@gmail.com>
2022-03-12 17:05:07 -0600

    Add body margin breakpoint variable to global styles

--- client/styles/global.ts
+++ client/styles/global.ts
@@ -18,6 +18,32 @@ const GlobalStyle = createGlobalStyle`
     --font-weight-default: 400;
 
     /************************/
+    /* Breakpoint Variables */
+    /************************/
+
+    /* Default mobile-first breakpoint */
+    --body-margin: 20px;
+
+    /* Small */
+    @media (min-width: 576px) { 
+    }
+
+    /* Medium */
+    @media (min-width: 768px) { 
+      --body-margin: 25px;
+    }
+
+    /* Large */
+    @media (min-width: 992px) { 
+      --body-margin: 30px;
+    }
+
+    /* X-Large */
+    @media (min-width: 1200px) { 
+      --body-margin: 35px;
+    }
+
+    /************************/
 
     box-sizing: border-box;
   }
@@ -31,6 +57,8 @@ const GlobalStyle = createGlobalStyle`
     color: var(--color-black);
     font-family: var(--font-primary);
     font-weight: var(--font-weight-default);
+
+    margin: var(--body-margin);
   }
 
   h1, h2, h3, h4, h5, h6 {
