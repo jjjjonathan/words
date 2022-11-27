5bbc989f3a5f8a99a07d9536ea466709ad96e235
jjjjonathan <hornathanjon@gmail.com>
2022-11-27 12:43:17 -0800

    codegen yml

---
+++ server/generated/graphql.ts
@@ -0,0 +1,248 @@
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  DateTime: any;
+  EmailAddress: any;
+};
+
+export type Blog = Timestamps & {
+  __typename?: 'Blog';
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Int'];
+  owner: User;
+  posts: Array<Maybe<Post>>;
+  slug: Scalars['String'];
+  subtitle?: Maybe<Scalars['String']>;
+  title: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type Post = Timestamps & {
+  __typename?: 'Post';
+  author: User;
+  blog: Blog;
+  body: Scalars['String'];
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Int'];
+  slug: Scalars['String'];
+  title: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  blog?: Maybe<Blog>;
+  post?: Maybe<Post>;
+  user?: Maybe<User>;
+};
+
+
+export type QueryBlogArgs = {
+  slug: Scalars['String'];
+};
+
+
+export type QueryPostArgs = {
+  slug: Scalars['String'];
+};
+
+
+export type QueryUserArgs = {
+  username: Scalars['String'];
+};
+
+export type Timestamps = {
+  createdAt: Scalars['DateTime'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type User = Timestamps & {
+  __typename?: 'User';
+  bio?: Maybe<Scalars['String']>;
+  blogs: Array<Maybe<Blog>>;
+  createdAt: Scalars['DateTime'];
+  email: Scalars['EmailAddress'];
+  firstName: Scalars['String'];
+  id: Scalars['Int'];
+  lastName: Scalars['String'];
+  location?: Maybe<Scalars['String']>;
+  posts: Array<Maybe<Post>>;
+  updatedAt: Scalars['DateTime'];
+  username: Scalars['String'];
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Blog: ResolverTypeWrapper<Blog>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  DateTime: ResolverTypeWrapper<Scalars['DateTime']>;
+  EmailAddress: ResolverTypeWrapper<Scalars['EmailAddress']>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  Post: ResolverTypeWrapper<Post>;
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  Timestamps: ResolversTypes['Blog'] | ResolversTypes['Post'] | ResolversTypes['User'];
+  User: ResolverTypeWrapper<User>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Blog: Blog;
+  Boolean: Scalars['Boolean'];
+  DateTime: Scalars['DateTime'];
+  EmailAddress: Scalars['EmailAddress'];
+  Int: Scalars['Int'];
+  Post: Post;
+  Query: {};
+  String: Scalars['String'];
+  Timestamps: ResolversParentTypes['Blog'] | ResolversParentTypes['Post'] | ResolversParentTypes['User'];
+  User: User;
+};
+
+export type BlogResolvers<ContextType = any, ParentType extends ResolversParentTypes['Blog'] = ResolversParentTypes['Blog']> = {
+  createdAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  owner?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  posts?: Resolver<Array<Maybe<ResolversTypes['Post']>>, ParentType, ContextType>;
+  slug?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  subtitle?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  updatedAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
+  name: 'DateTime';
+}
+
+export interface EmailAddressScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['EmailAddress'], any> {
+  name: 'EmailAddress';
+}
+
+export type PostResolvers<ContextType = any, ParentType extends ResolversParentTypes['Post'] = ResolversParentTypes['Post']> = {
+  author?: Resolver<ResolversTypes['User'], ParentType, ContextType>;
+  blog?: Resolver<ResolversTypes['Blog'], ParentType, ContextType>;
+  body?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  slug?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  updatedAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  blog?: Resolver<Maybe<ResolversTypes['Blog']>, ParentType, ContextType, RequireFields<QueryBlogArgs, 'slug'>>;
+  post?: Resolver<Maybe<ResolversTypes['Post']>, ParentType, ContextType, RequireFields<QueryPostArgs, 'slug'>>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'username'>>;
+};
+
+export type TimestampsResolvers<ContextType = any, ParentType extends ResolversParentTypes['Timestamps'] = ResolversParentTypes['Timestamps']> = {
+  __resolveType: TypeResolveFn<'Blog' | 'Post' | 'User', ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  updatedAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+};
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+  bio?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  blogs?: Resolver<Array<Maybe<ResolversTypes['Blog']>>, ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['EmailAddress'], ParentType, ContextType>;
+  firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  location?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  posts?: Resolver<Array<Maybe<ResolversTypes['Post']>>, ParentType, ContextType>;
+  updatedAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Blog?: BlogResolvers<ContextType>;
+  DateTime?: GraphQLScalarType;
+  EmailAddress?: GraphQLScalarType;
+  Post?: PostResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  Timestamps?: TimestampsResolvers<ContextType>;
+  User?: UserResolvers<ContextType>;
+};
+
---
+++ server/oldcodegen.ts
@@ -0,0 +1,99 @@
+// // Some imports are devDependencies. This file is only run in dev
+// /* eslint-disable import/no-extraneous-dependencies */
+
+// import * as fs from "fs/promises";
+// import * as path from "path";
+// import { loadFiles } from "@graphql-tools/load-files";
+// import { mergeTypeDefs } from "@graphql-tools/merge";
+// import { print } from "graphql";
+// import { Types as CodegenPluginTypes } from "@graphql-codegen/plugin-helpers";
+// import * as addPlugin from "@graphql-codegen/add";
+// import * as typescriptPlugin from "@graphql-codegen/typescript";
+// import * as typescriptResolversPlugin from "@graphql-codegen/typescript-resolvers";
+// import { codegen } from "@graphql-codegen/core";
+
+// // Set paths
+// const typeDefsDir = "./type-defs";
+// const typeDefsOutputFile = "./generated/all-type-defs.ts";
+// const resolverTypesOutputFile = "./generated/resolver-types.ts";
+
+// async function generate() {
+//   console.log("Starting GraphQL code generation...");
+//   console.log(`Loading typeDefs from ${typeDefsDir}`);
+
+//   // Load and merge all typeDefs
+//   const typeDefsArray = await loadFiles(path.join(__dirname, typeDefsDir));
+//   const allTypeDefs = mergeTypeDefs(typeDefsArray);
+
+//   console.log("Compiling typeDefs...");
+
+//   // Output all typeDefs to TS file
+//   const printedTypeDefs = print(allTypeDefs);
+//   const typeDefsOutput = `export default \`\n${printedTypeDefs}\n\`;`;
+//   const absoluteTypeDefsOutputPath = path.join(__dirname, typeDefsOutputFile);
+//   await fs.writeFile(absoluteTypeDefsOutputPath, typeDefsOutput);
+
+//   console.log(`All typeDefs outputted to \n\t ${absoluteTypeDefsOutputPath}`);
+
+//   // This type necessary for multiple plugins to work.
+//   // Make sure each plugin is its own object in plugins array.
+//   const config: CodegenPluginTypes.GenerateOptions = {
+//     documents: [],
+//     config: {
+//       contextType: "MyContext",
+//       mappers: {
+//         User: "../db/queries/user#GQLUserModel",
+//         Blog: "../db/queries/blog#GQLBlogModel",
+//         Post: "../db/queries/post#GQLPostModel",
+//       },
+//       scalars: {
+//         DateTime: "Date",
+//         EmailAddress: "string",
+//       },
+//     },
+//     // filename is used by a plugin internally. The 'typescript' plugin
+//     // returns the string output rather than writing to a file
+//     filename: resolverTypesOutputFile,
+//     schema: allTypeDefs,
+//     plugins: [
+//       {
+//         add: {
+//           // This must match the contextType in config above to properly import and use the context
+//           // Path is relative to "generated" folder
+//           content: "import { MyContext } from '../context';",
+//         },
+//       },
+//       {
+//         typescript: {},
+//       },
+//       {
+//         typescriptResolvers: { useIndexSignature: true },
+//       },
+//       // TODO add 'typescript-react-apollo', 'typescript-operations',
+//       // and maybe 'introspection' plugins for frontend gen.
+//       // Write script to copy files to client folder.
+//     ],
+//     pluginMap: {
+//       add: addPlugin,
+//       typescript: typescriptPlugin,
+//       typescriptResolvers: typescriptResolversPlugin,
+//     },
+//   };
+
+//   console.log("Generating TypeScript definitions...");
+
+//   // Output TS defs to string and write to file
+//   const output = await codegen(config);
+//   const absoluteResolverTypesOutputPath = path.join(
+//     __dirname,
+//     resolverTypesOutputFile
+//   );
+//   await fs.writeFile(absoluteResolverTypesOutputPath, output);
+
+//   console.log(
+//     `TypeScript definitions for resolvers outputted to \n\t ${absoluteResolverTypesOutputPath}`
+//   );
+//   console.log("Code generation complete!");
+// }
+
+// generate();
