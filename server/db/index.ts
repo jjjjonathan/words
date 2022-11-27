960ac18255d868b2e464aa15f83797f8dc755a1b
jjjjonathan <hornathanjon@gmail.com>
2022-11-27 10:43:49 -0800

    add basic apollo server

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,6 @@
-import { Pool } from "pg";
+import pg from "pg";
 import { user, host, database, password, port } from "../config";
 
-const pool = new Pool({ user, host, database, password, port });
+const pool = new pg.Pool({ user, host, database, password, port });
 
 export default pool;
